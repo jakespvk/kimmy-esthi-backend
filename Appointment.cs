4186993410b54ed105a0b628ef629ae150a79726
Jake Spievak <jakespvk@gmail.com>
2025-03-09 15:17:36 -0700

    idk

--- Appointment.cs
+++ Appointment.cs
@@ -1,22 +1,9 @@
-public class Appointment {
-
-    private int _id;
-    private string _date;
-    private string _time;
-    private string _status;
-
-    /*
-    public Appointment(int id, string date, string time, string status) {
-        _id = id;
-        _date = date;
-        _time = time;
-        _status = status;
-    }
-    */
-
-    public int Id { get { return _id; } }
-    public string Date { get { return _date; } }
-    public string Time { get { return _time; } }
-    public string Status { get { return _status; } }
+public class Appointment
+{
+	[System.ComponentModel.DataAnnotations.Key]
+    public int Id { get; }
+    public string? Date { get; }
+    public string? Time { get; }
+    public string? Status { get; }
 }
-    
+
