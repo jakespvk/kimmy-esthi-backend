c7ec85101b60e95b29bbf4111a42e02ee086de86
Jake Spievak <jakespvk@gmail.com>
2025-12-28 21:22:22 -0800

    update

--- AppointmentDb.cs
+++ AppointmentDb.cs
@@ -19,6 +19,8 @@ public class AppointmentDb : DbContext
             .WithOne()
             .HasForeignKey<ScheduledAppointment>(sa => sa.AppointmentId);
 
+        modelBuilder.Ignore<AppointmentRequest>();
+
         base.OnModelCreating(modelBuilder);
     }
 }
